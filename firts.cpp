b66fe25bba356ece7bbc6e11cd1e135cb133a358
HARSHIL2023 <harshil.patel.cg@gmail.com>
2026-02-02 14:19:33 +0530

    chnages and expalined things

--- firts.cpp
+++ firts.cpp
@@ -1,5 +1,8 @@
 #include<iostream> 
 using namespace std;
 int main(){
-    cout << "hello i am harshil patel" <<endl;  
+    cout << "hello i am harshil patel" <<endl ; 
+    cout <<"endl end statement and print next things in second line" <<endl ;
+    cout << "second line chekcking spaces" <<endl ;
+
 }
