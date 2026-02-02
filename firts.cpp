9332ac82d6a0e54b6845b4a5639280dd432f4503
HARSHIL2023 <harshil.patel.cg@gmail.com>
2026-02-02 14:26:29 +0530

    code writen for user input

--- firts.cpp
+++ firts.cpp
@@ -1,8 +1,12 @@
 #include<iostream> 
 using namespace std;
-int main(){
+int main()
+{
+    int a;
     cout << "hello i am harshil patel" <<endl ; 
     cout <<"endl end statement and print next things in second line" <<endl ;
     cout << "second line chekcking spaces" <<endl ;
+    cin >>a;
+    cout<< a;
 
 }
