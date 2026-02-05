d62959457532d17ea7d9d9241fbc03e0ba1e39bf
HARSHIL2023 <harshil.patel.cg@gmail.com>
2026-02-05 15:19:03 +0530

    added file handling things

---
+++ filehandlig.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+#include<fstream>
+
+using namespace std;
+
+int main(){
+
+ofstream file;
+
+file.open("sample.txt",ios::app);
+
+if(file.is_open()){
+file << "First time using the file operation with the help of ofstream mmethod." << endl;
+file.close();
+
+}
+
+return 0;
+}
