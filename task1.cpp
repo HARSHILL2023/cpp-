e11ac5a2435aa744c893f2887444055f4cf744ee
HARSHIL2023 <harshil.patel.cg@gmail.com>
2026-02-10 13:33:27 +0530

    sss

---
+++ task1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   int n;
+
+   cout << "entyer a value"<<endl;
+    cin >>n;
+    if(n>0){
+        cout<<"postitive"<<endl;
+    }
+    else{
+        cout<<"negasitve"<<endl;
+    }
+
+
+    return 0;
+}
